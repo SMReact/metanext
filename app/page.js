00528a70a8baa2147b264d6001b309ff7ab46303
shakiba-yazdani <shakibayazdaniii@gmail.com>
2023-05-06 13:06:05 +0330

    update tailwind.config.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div>
+  <div className='bg-black overflow-hidden' >
     <Navbar />
     <Hero />
     <About />
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#c7c7c7',
+      },
+      transitionTimingFunction: {
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
+      },
     },
   },
   plugins: [],
